8adb632f37bee9f4173698cafc835e73b16af7ca
HANBANG <HANBANG@HANBANGdeMacBook-Pro.local>
2015-05-13 20:52:34 +0800

    Initial Commit

---
+++ wjdj/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  wjdj
+//
+//  Created by HANBANG on 15/5/13.
+//  Copyright (c) 2015年 HANBANG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
